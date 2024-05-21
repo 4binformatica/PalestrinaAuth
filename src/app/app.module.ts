5d625bd5644f5b0dbcb8022ef09bdc52fdf31a5d
Mirko Magnani <mirko.magna010@gmail.com>
2024-05-21 08:55:25 +0200

    github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-
+import { AppComponent } from './app/app.component';
+import { Login } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    Login
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
